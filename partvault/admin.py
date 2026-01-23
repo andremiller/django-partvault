8ccde7e8bb6a425c45e8ada9b34010131c8449c7
Andre Miller <3374093+andremiller@users.noreply.github.com>
2026-01-23 21:55:51 +0200

    Updated Item model fields and added related models

--- partvault/admin.py
+++ partvault/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Collection, Item
+from .models import Category, Collection, Item, Manufacturer, Status, Tag
 
 admin.site.register(Collection)
+admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Manufacturer)
+admin.site.register(Status)
+admin.site.register(Tag)
--- partvault/models.py
+++ partvault/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+# TODO Add unique constraints
+# TODO Add collection memberships (via CollectionMembership table, viewer, editor, admin)
+
 
 class Collection(models.Model):
     """A collection of items"""
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
     asset_tag_prefix = models.CharField(max_length=2)
     is_public = models.BooleanField(default=False)
 
@@ -14,15 +17,75 @@ class Collection(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Tag(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Manufacturer(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Status(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+
+    class Meta:
+        verbose_name_plural = "statuses"
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Item(models.Model):
+    # TODO Add index
+    # TODO Add validation to ensure related fields belong to same collection (clean)
+
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    category = models.ForeignKey(
+        Category, null=True, blank=True, on_delete=models.SET_NULL
+    )
+    manufacturer = models.ForeignKey(
+        Manufacturer, null=True, blank=True, on_delete=models.SET_NULL
+    )
+    model = models.CharField(max_length=120, blank=True)
+    revision = models.CharField(max_length=120, blank=True)
+    serial = models.CharField(
+        max_length=120, blank=True
+    )  # TODO Replace with m2m identifier table to cater for different identifier types
+    status = models.ForeignKey(Status, null=True, blank=True, on_delete=models.SET_NULL)
+    parent_item = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="contained_items",
+    )
+    notes = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name="items")
+    acquired_on = models.DateField(blank=True, null=True)
+    last_tested_on = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)    
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
-
-    
-    
