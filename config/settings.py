526a8c90cf4437c06aeab6bc10fa953d73486bb1
Andre Miller <3374093+andremiller@users.noreply.github.com>
2026-01-24 23:29:46 +0200

    Bootstrap5 templates

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_bootstrap5',
     'partvault.apps.PartvaultConfig',
     'django.contrib.admin',
     'django.contrib.auth',
